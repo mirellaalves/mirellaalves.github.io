445187771f5f5ee43526f5946da0bfe5bdff3648
mirellaalves <mirella.alves@gmail.com>
2021-01-27 14:48:16 -0300

    create files and notes ex3, ex4 and ex5 bonus

---
+++ exercises/35_2/bonus_ex5.py
@@ -0,0 +1,12 @@
+# Bônus
+# Exercício 5: Utilizando o arquivo pokemons.json , vamos escrever um programa
+# que sorteie um pokemon aleatoriamente. O programa deve perguntar à pessoa
+# usuária "Quem é esse pokemon?", até que ela o acerte. A cada erro, apresente
+# um número de letras do nome daquele pokemon igual ao número de erros.
+# Exemplo: o pokemon sorteado pelo programa é butterfree, a pessoa usuária
+# chuta charizard, o programa deve exibir b. Em seguida, a pessoa chuta
+# blastoise, o programa deve exibir bu e assim por diante até a pessoa
+# acertar.
+# Você pode utilizar o método choice do modulo random para sortear
+# aleatoriamente um pokemon.
+# Ex: random.choice(pokemon) -> Saída: {"name": "Pikachu", }
---
+++ exercises/35_2/ex3.py
@@ -0,0 +1,2 @@
+# Exercício 3: Modifique o exercício anterior para que as palavras sejam lidas
+# de um arquivo. Considere que o arquivo terá cada animal em uma linha.
